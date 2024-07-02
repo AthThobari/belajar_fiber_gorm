3b204c96ab6459d86dbe12d0f774e0c9edffdf72
AthThobari <thobari.mutu@gmail.com>
2024-07-02 17:28:27 +0700

    feat: menambahkan middleware

--- route/index.route.go
+++ route/index.route.go
@@ -7,10 +7,21 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func Middelware(ctx *fiber.Ctx) error {
+	token := ctx.Get("x-token")
+
+	if token != "secret" {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "unautenticated",
+		})
+	}
+	return ctx.Next()
+}
+
 func RouteInit(r *fiber.App) {
 	
 	r.Static("/public", config.ProjectRootPath+"/public/asset")
-	r.Get("/user",handler.UserHandlerGetAll)
+	r.Get("/user",Middelware, handler.UserHandlerGetAll)
 	r.Get("/user/:id", handler.UserHandlerGetByID)
 	r.Post("/user",handler.UserHandlerCreate)
 	r.Put("/user/:id",handler.UserHandlerUpdate)
