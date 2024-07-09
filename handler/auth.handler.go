9744ece6b7e88a139de7f976c1a7c963b614480c
AthThobari <thobari.mutu@gmail.com>
2024-07-09 10:15:43 +0700

    feat: menambahkan generate JWT

--- handler/auth.handler.go
+++ handler/auth.handler.go
@@ -5,7 +5,10 @@ import (
 	"belajar_golang_fiber/model/entity"
 	"belajar_golang_fiber/model/request"
 	"belajar_golang_fiber/utils"
+	"log"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
@@ -44,7 +47,23 @@ func LoginHandler(ctx *fiber.Ctx) error {
 		})
 	}
 
+	// GENERATE JWT
+	claims := jwt.MapClaims{}
+	claims["name"] = user.Name
+	claims["email"] = user.Email
+	claims["address"] = user.Address
+	claims["exp"] = time.Now().Add(time.Minute * 2).Unix()
+
+	token, errGenerateToken := utils.GenerateToken(&claims)
+
+	if errGenerateToken != nil {
+		log.Println(errGenerateToken)
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "wrong credential",
+		})
+	}
+
 	return ctx.JSON(fiber.Map{
-		"token": "secret",
+		"token": token,
 	})
 }
